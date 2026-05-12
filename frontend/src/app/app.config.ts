30933e7cb7ef1730db984fd4311a56e756fdfec1
r-happy <106812882+r-happy@users.noreply.github.com>
2026-05-12 12:40:56 +0900

    fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,16 +6,11 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
   ],
 };
